d7afa99f05f80099a9a234d079887dbaaaa09397
river0077 <giang02112006@gmail.com>
2025-09-16 23:42:37 +0700

    update Script and fix UI

--- src/AppUI.java
+++ src/AppUI.java
@@ -6,8 +6,11 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Image;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -36,6 +39,7 @@ public class AppUI extends JFrame {
         buttonsUp = new JButton[4];
         for (int i = 0; i < buttonsUp.length; i++) {
             buttonsUp[i] = setImgforButton("resources/up.png");
+            buttonsUp[i].addActionListener(new ButtonProcess());
             panelUp.add(buttonsUp[i]);
         }
         panelCenter.add(panelUp);
@@ -49,7 +53,7 @@ public class AppUI extends JFrame {
             textFields[i].setText("0");
             textFields[i].setFont(textFields[i].getFont().deriveFont(30f));
             textFields[i].setEnabled(false);
-            textFields[i].setDisabledTextColor(Color.BLACK);  // chữ vẫn đen
+            textFields[i].setDisabledTextColor(Color.BLACK);
             textFields[i].setBackground(Color.WHITE); 
             panelTextField.add(textFields[i]);
         }
@@ -58,12 +62,15 @@ public class AppUI extends JFrame {
         buttonsDown = new JButton[4];
         for (int i = 0; i < buttonsDown.length; i++) {
             buttonsDown[i] = setImgforButton("resources/down.png");
+            buttonsDown[i].addActionListener(new ButtonProcess());
             panelDown.add(buttonsDown[i]);
         }
         panelCenter.add(panelDown);
         panelSelect = new JPanel();
         buttonCheck = new JButton("Check");
         buttonReset = new JButton("Reset");
+        buttonCheck.addActionListener(new ButtonProcess());
+        buttonReset.addActionListener(new ButtonProcess());
         panelSelect.add(buttonCheck);
         panelSelect.add(buttonReset);
         Container window = getContentPane();
@@ -85,6 +92,47 @@ public class AppUI extends JFrame {
         } catch (Exception e) {
             e.printStackTrace();
             return null;
+        }
+    }
+    private class ButtonProcess implements ActionListener {
+        @Override
+        public void actionPerformed(java.awt.event.ActionEvent e) {
+            JButton button = (JButton) e.getSource();
+            for(int i = 0; i < buttonsUp.length; i++) {
+                if(button == buttonsUp[i]) {
+                    int currentValue = Integer.parseInt(textFields[i].getText());
+                    currentValue++;
+                    if(currentValue > 9) currentValue = 0;
+                    textFields[i].setText(String.valueOf(currentValue));
+                    textFields[i].setDisabledTextColor(Color.BLACK);
+                } else if(button == buttonsDown[i]) {
+                    int currentValue = Integer.parseInt(textFields[i].getText());
+                    currentValue--;
+                    if(currentValue < 0) currentValue = 9;
+                    textFields[i].setText(String.valueOf(currentValue));
+                    textFields[i].setDisabledTextColor(Color.BLACK);
+                } else if (button == buttonCheck) {
+                    Script script = new Script();
+                    if(script.ifDuplicateNumbers(textFields)) {
+                        JOptionPane.showMessageDialog(null, "Duplicate numbers are not allowed!", "Warning", JOptionPane.WARNING_MESSAGE);
+                        return;
+                    }
+                    textFields = script.checkPassword(textFields);
+                    if(script.rightPassword(textFields)) {
+                        JOptionPane.showMessageDialog(null, "Congratulations! You guessed the correct pin code!", "Success", JOptionPane.INFORMATION_MESSAGE);
+                        revalidate();
+                        repaint();
+                        return;
+                    }
+                    revalidate();
+                    repaint();
+                } else if (button == buttonReset) {
+                    for(int j = 0; j < textFields.length; j++) {
+                        textFields[j].setText("0");
+                        textFields[j].setDisabledTextColor(Color.BLACK);
+                    }
+                }
+            }
         }
     }
 }
--- src/Script.java
+++ src/Script.java
@@ -1,3 +1,68 @@
+import java.awt.Color;
+
+import javax.swing.JTextField;
+
 public class Script {
-    
+    int[] password = {3, 1, 4, 2};
+    public JTextField[] checkPassword(JTextField[] fields) {
+        for (int i = 0; i < fields.length; i++) {
+            int value = Integer.parseInt(fields[i].getText());
+            if (value == password[i]) {
+                rightPosition(fields[i]);
+            } else if (value != password[i] && contains(password, value)) {
+                wrongPosition(fields[i]);
+            } else {
+                notInPassword(fields[i]);
+            }
+        }
+        return fields;
+    }
+    public boolean rightPassword(JTextField[] fields) {
+        for (int i = 0; i < fields.length; i++) {
+            int value = Integer.parseInt(fields[i].getText());
+            if (value != password[i]) {
+                return false;
+            }
+        }
+        for (JTextField field : fields) {
+            field.setDisabledTextColor(Color.GREEN);
+        }
+        return true;
+    }
+    public boolean contains(int[] password2, int value) {
+        for (int i : password2) {
+            if (i == value) return true;
+        }
+        return false;
+    }
+    public void rightPosition(JTextField field) {
+        field.setDisabledTextColor(Color.GREEN);
+
+    }
+    public void wrongPosition(JTextField field) {
+        field.setDisabledTextColor(Color.YELLOW);
+        
+    }
+    public void notInPassword(JTextField field) {
+        field.setDisabledTextColor(Color.RED);
+        
+    }
+    public boolean ifDuplicateNumbers(JTextField[] fields) {
+        int[] num = new int[4];
+        for (int i = 0; i < fields.length; i++) {
+            num[i] = Integer.parseInt(fields[i].getText());
+        }
+        boolean check = false;
+        for (int i = 0; i < num.length; i++) {
+            outer:for (int j = i + 1; j < num.length; j++) {
+                if (num[i] != num[j]) {
+                    continue outer;
+                } else {
+                    check = true;
+                }
+            }
+        }
+        return check;
+    }
+
 }
